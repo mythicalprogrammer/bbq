9104a857fb3319304c71869f61a66d0f17cc75d6
xtine <piroko@gmail.com>
2010-04-23 01:19:39 -0700

    Adding Routes file

---
+++ app/config/routes.php
@@ -0,0 +1,58 @@
+<?php
+/* SVN FILE: $Id$ */
+/**
+ * Short description for file.
+ *
+ * In this file, you set up routes to your controllers and their actions.
+ * Routes are very important mechanism that allows you to freely connect
+ * different urls to chosen controllers and their actions (functions).
+ *
+ * PHP versions 4 and 5
+ *
+ * CakePHP(tm) :  Rapid Development Framework (http://www.cakephp.org)
+ * Copyright 2005-2008, Cake Software Foundation, Inc. (http://www.cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @filesource
+ * @copyright     Copyright 2005-2008, Cake Software Foundation, Inc. (http://www.cakefoundation.org)
+ * @link          http://www.cakefoundation.org/projects/info/cakephp CakePHP(tm) Project
+ * @package       cake
+ * @subpackage    cake.app.config
+ * @since         CakePHP(tm) v 0.2.9
+ * @version       $Revision$
+ * @modifiedby    $LastChangedBy$
+ * @lastmodified  $Date$
+ * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
+ */
+/**
+ * Here, we are connecting '/' (base path) to controller called 'Pages',
+ * its action called 'display', and we pass a param to select the view file
+ * to use (in this case, /app/views/pages/home.ctp)...
+ */
+	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+/**
+ * ...and connect the rest of 'Pages' controller's urls.
+ */
+	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
+	
+	Router::connect('/admin/', array('controller' => 'users', 'action' => 'login'));
+	Router::connect('/admin/joints', array('controller' => 'users', 'action' => 'login'));
+	
+	// Login, Logout
+	Router::connect('/login', array('controller' => 'users', 'action' => 'login'));
+	Router::connect('/admin/logout', array('controller' => 'users', 'action' => 'logout'));
+
+	// BBQ Joints Landing Page
+	Router::connect('/joints/',array('controller'=>'state'));
+	Router::connect('/joint/',array('controller'=>'state'));
+	
+	// States Listing
+	Router::connect('/joints/*',array('controller'=>'state','action' => 'showState'));
+	
+	// BBQ Joint Listing
+	Router::connect('/joint/:id/:slug', array('controller' => 'joints', 'action' => 'view'), array('pass' => array('id', 'slug'), 'id' => '[\d]+' ));
+	
+	
+?>
